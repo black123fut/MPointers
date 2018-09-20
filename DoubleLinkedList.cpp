a20c54d778b484121ac34af7b6d01af278d3b3d7
isaac <isaacbenavides20115@gmail.com>
2018-09-19 18:27:13 -0600

    Algoritmos de ordenamiento

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -1,76 +1,109 @@
-//
-// Created by isaac on 19/09/18.
-//
-
+#include <iostream>
 #include "MPointer.h"
 
+using namespace std;
+
+
+template <class T>
 class DoubleNode{
 public:
+
     DoubleNode(MPointer<int> data){
         this->data = data;
+        next = nullptr;
+        prev = nullptr;
     }
 
     DoubleNode(){
+        data = NULL;
         next = nullptr;
         prev = nullptr;
-    };
+    }
 
-    const MPointer<int> &getData() const {
+    MPointer<int> &getData() {
         return data;
     }
 
-    void setData(const MPointer<int> &data) {
-        DoubleNode::data = data;
+    void setData(MPointer<int> data) {
+        this->data = data;
     }
 
-    DoubleNode *getNext() const {
-        return next;
-    }
-
-    void setNext(DoubleNode *next) {
-        DoubleNode::next = next;
-    }
-
-    DoubleNode *getPrev() const {
+    DoubleNode<T> *getPrev() {
         return prev;
     }
 
-    void setPrev(DoubleNode *prev) {
-        DoubleNode::prev = prev;
+    void setPrev(DoubleNode<T> *prev) {
+        this->prev = prev;
+    }
+
+    void setNext(DoubleNode<T> *next){
+        this->next = next;
+    }
+
+    DoubleNode<T> *getNext() {
+        return next;
     }
 
 private:
     MPointer<int> data;
-    DoubleNode *next;
-    DoubleNode *prev;
+    DoubleNode<T> *prev;
+    DoubleNode<T> *next;
 };
 
 class DoubleLinkedList{
 public:
     DoubleLinkedList(){
         head = nullptr;
+        size = 0;
     }
 
-    void add(MPointer<int> dato){
+    void add(MPointer<int> data){
         if (head == nullptr){
-            head = new DoubleNode(dato);
-            size++;
+            head = new DoubleNode< MPointer<int> >(data);
         } else {
-            DoubleNode *tmp = head;
-            while(tmp->getNext() != nullptr){
+            DoubleNode< MPointer<int> > *tmp = head;
+
+            while (tmp->getNext() != nullptr){
                 tmp = tmp->getNext();
             }
-            DoubleNode *newNode = new DoubleNode(dato);
+            DoubleNode< MPointer<int> > *newNode = new DoubleNode< MPointer<int> >(data);
             tmp->setNext(newNode);
             newNode->setPrev(tmp);
-            size++;
+        }
+        size++;
+    }
+
+    void remove(int i){
+        if (i == 0){
+            DoubleNode< MPointer<int> > *tmp = head;
+            head = head->getNext();
+            head->setPrev(nullptr);
+            free(tmp);
+            size--;
+        }
+        else {
+            DoubleNode< MPointer<int> > *tmp = head;
+            int cont = 1;
+            while (tmp->getNext() != nullptr){
+                if (i == cont){
+                    tmp->setNext(tmp->getNext()->getNext());
+                    if (tmp->getNext() != nullptr)
+                        tmp->getNext()->setPrev(tmp);
+                    size--;
+                    return;
+                } else {
+                    cont++;
+                    tmp = tmp->getNext();
+                }
+            }
         }
     }
 
-    MPointer<int> get(int index){
-        if (index < size){
-            DoubleNode *tmp = head;
-            for (int i = 0; i < index; ++i) {
+    MPointer<int> get(int i){
+        if (i < size){
+            DoubleNode< MPointer<int> > *tmp = head;
+
+            for (int j = 0; j < i; ++j) {
                 tmp = tmp->getNext();
             }
             return tmp->getData();
@@ -82,6 +115,6 @@ public:
     }
 
 private:
-    DoubleNode *head;
+    DoubleNode< MPointer<int> > *head;
     int size;
 };
--- Sort.cpp
+++ Sort.cpp
@@ -1,7 +1,3 @@
-//
-// Created by isaac on 19/09/18.
-//
-
 #include <iostream>
 #include "DoubleLinkedList.cpp"
 
@@ -9,22 +5,61 @@ using namespace std;
 
 class Sort{
 public:
-    void quickSort(DoubleLinkedList *list, int left, int rigth);
+    void bubblesort(DoubleLinkedList *list, int length){
+        for (int i = 0; i < length - 1; i++) {
+
+            for (int j = 0; j < length - 1 - i; ++j) {
+                if (&list->get(j) > &list->get(j + 1)){
+                    MPointer<int> tmp = MPointer<int>::New();
+                    tmp = list->get(j);
+                    list->get(j) = list->get(j + 1);
+                    list->get(j + 1) = tmp;
+                }
+            }
+        }
+    }
+
+    void quickSort(DoubleLinkedList *list, int left, int length){
+        int i = left, j = length;
+        MPointer<int> tmp = MPointer<int>::New();
+        MPointer<int> pivot = list->get((left + length) / 2);
+
+        while (i <= j) {
+            while (&list->get(i) < &pivot)
+                i++;
+            while (&list->get(j) > &pivot)
+                j--;
+            if (i <= j){
+                tmp = list->get(i);
+                list->get(i) = list->get(j);
+                list->get(j) = tmp;
+                i++;
+                j--;
+            }
+        }
+
+        if (left < j)
+            quickSort(list, left, j);
+        if (i < length)
+            quickSort(list, i, length);
+
+    }
 
     void insertionSort(DoubleLinkedList *list, int length){
         int i, j;
-        MPointer<int> pivot = MPointer<int>::New();
+        MPointer<int> key = MPointer<int>::New();
 
         for (i = 0; i < length; i++) {
-            pivot = list->get(i);
-            j = i -1;
+            key = list->get(i);
+            j = i - 1;
 
-            while (j >= 0 && &list->get(j) > &pivot){
+            while (j >= 0 && &list->get(j) > &key) {
                 list->get(j + 1) = list->get(j);
-                j = j -1;
+                j = j - 1;
             }
-            list->get(j + 1) = pivot;
+            list->get(j + 1) = key;
         }
     }
+private:
 
 };
